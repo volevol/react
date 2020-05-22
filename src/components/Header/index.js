6c1f32d2086cb0d16cf05a862489ddd1b2a0c40a
Ivan Fedorusov <ifedorusov@gmail.com>
2020-05-22 13:06:40 +0300

    ok

--- src/components/Header/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './Header.module.scss';
-
-const Header = ({ children }) => {
-    return <h1 className={s.header}>{children}</h1>;
-};
-
-export default Header;
